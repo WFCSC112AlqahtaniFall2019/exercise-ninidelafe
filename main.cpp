6eeb3958e115d971577f44458252aaba32992b72
ninidelafe <delaam18@wfu.edu>
2019-09-05 16:16:49 -0400

    Swapped Vectors

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
+    vector<int>revVctr(8);    // creating vector
     int i;                        // Loop index
     int temp=0;                   //Temporary holder for values
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
     }
 
     // Reverse
     for (i = 0; i <=NUM_ELEMENTS/2; ++ i) {
-        temp=revVctr[i];
-        revVctr[i]=revVctr[NUM_ELEMENTS-(i+1)];
-        revVctr[NUM_ELEMENTS-(i+1)]=temp;// Swap
+        temp=revVctr.at(i);
+        revVctr.at(i)=revVctr.at(NUM_ELEMENTS-(i+1));
+        revVctr.at(NUM_ELEMENTS-(i+1))=temp;// Swap
     }
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr.at(i);
     }
     cout << endl;
 
